5a1167ae480633a65febe1cf1bfc7838de4c02f0
Hobar <huparelaa@eafit.edu.co>
2023-07-22 09:18:14 -0500

    Responsive grid design added

--- src/components/games/Games.jsx
+++ src/components/games/Games.jsx
@@ -2,30 +2,32 @@ import "./games.css";
 export function Games({ games }) {
   return (
     <>
-      <div className="games-container">
-        {games.map((game) => (
-          <div key={game.id} className="game-card">
-            <p className="title">{game.name}</p>
-            <div className="img-container">
-              <img src={game.background_image} alt={game.name} />
+      <div className="entire-component">
+        <div className="games-container">
+          {games.map((game) => (
+            <div key={game.id} className="game-card">
+              <p className="title">{game.name}</p>
+              <div className="img-container">
+                <img src={game.background_image} alt={game.name} />
+              </div>
+              <p
+                className="rating"
+                style={{
+                  background:
+                    game.rating > 4.6
+                      ? "darkgreen"
+                      : game.rating > 4.0
+                      ? "green"
+                      : game.rating > 3.0
+                      ? "orange"
+                      : "red",
+                }}
+              >
+                {parseInt(game.rating * 20)}
+              </p>
             </div>
-            <p
-              className="rating"
-              style={{
-                background:
-                  game.rating > 4.6
-                    ? "darkgreen"
-                    : game.rating > 4.0
-                    ? "green"
-                    : game.rating > 3.0
-                    ? "orange"
-                    : "red",
-              }}
-            >
-              {parseInt(game.rating * 20)}
-            </p>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </>
   );
